8caa15fe5344ddfeefee4d134c30333099bd1898
mahmouddessoki <mahmouddessoki33@gmail.com>
2025-11-15 18:43:11 +0200

    move components from core to shared

--- apps/Fitness/src/app/features/pages/home/components/meals/meals.ts
+++ apps/Fitness/src/app/features/pages/home/components/meals/meals.ts
@@ -1,19 +1,18 @@
 import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
-import { MainCard } from './../../../../../shared/components/ui/main-card/main-card';
-import { Title } from './../../../../../core/components/title/title';
-import { Header } from './../../../../../core/components/header/header';
-import { MealService } from './../../../../../shared/services/meals/meals';
-import { Carousel } from "./../../../../../shared/components/ui/carousel/carousel";
 import { Category } from './../../../../../shared/models/meals';
+import { MealService } from './../../../../../shared/services/meals/meals';
+//reusable
+import { Carousel } from './../../../../../shared/components/ui/carousel/carousel';
+import { Header } from './../../../../../shared/components/ui/header/header';
+import { Title } from './../../../../../shared/components/ui/title/title';
 
 @Component({
   selector: 'app-meals',
-  imports: [MainCard, Title, Header, Carousel],
+  imports: [Title, Header, Carousel],
   templateUrl: './meals.html',
   styleUrl: './meals.scss',
 })
 export class Meals implements OnInit {
-  x = [1, 2, 3];
   private mealService = inject(MealService);
   mealCats: WritableSignal<Category[]> = signal([]);
 
@@ -23,7 +22,7 @@ export class Meals implements OnInit {
   getMealCats() {
     this.mealService.getMealsCats().subscribe({
       next: (res) => {
-        this.mealCats.set(res.categories)
+        this.mealCats.set(res.categories);
       },
     });
   }
