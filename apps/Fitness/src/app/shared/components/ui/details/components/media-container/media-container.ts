8c386434a791a38a23416de3f5085aaf5f7b77c1
mahmouddessoki <mahmouddessoki33@gmail.com>
2025-11-27 00:55:10 +0200

    details right section static UI completed

--- apps/Fitness/src/app/shared/components/ui/details/components/media-container/media-container.ts
+++ apps/Fitness/src/app/shared/components/ui/details/components/media-container/media-container.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {Component, input} from "@angular/core";
 
 @Component({
     selector: "app-media-container",
@@ -6,4 +6,6 @@ import {Component} from "@angular/core";
     templateUrl: "./media-container.html",
     styleUrl: "./media-container.scss",
 })
-export class MediaContainer {}
+export class MediaContainer {
+  type = input.required<'meal'|'class'>()
+}
