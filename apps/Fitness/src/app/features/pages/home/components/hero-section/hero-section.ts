6cb296fed6ac629dd1ee93ca660335effed49a97
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-12-02 07:02:38 +0200

    feat: 🎸 (hero)add translation, handle rtl and refactor buttons

--- apps/Fitness/src/app/features/pages/home/components/hero-section/hero-section.ts
+++ apps/Fitness/src/app/features/pages/home/components/hero-section/hero-section.ts
@@ -1,26 +1,27 @@
-import { Component } from "@angular/core";
-import { Header } from "./../../../../../shared//components/ui/header/header";
-import { MainButton } from "./../../../../../shared/components/ui/main-button/main-button";
+import {Component} from "@angular/core";
+import {Header} from "./../../../../../shared//components/ui/header/header";
+import {MainButton} from "./../../../../../shared/components/ui/main-button/main-button";
+import {TranslatePipe} from "@ngx-translate/core";
 
 @Component({
     selector: "app-hero-section",
-    imports: [Header, MainButton],
+    imports: [Header, MainButton, TranslatePipe],
     templateUrl: "./hero-section.html",
     styleUrl: "./hero-section.scss",
 })
 export class HeroSection {
     statistics = [
         {
-            num: 1200,
-            desc: "Active Members",
+            num: "hero.statistics.index0.number",
+            desc: "hero.statistics.index0.text",
         },
         {
-            num: 12,
-            desc: "Certified Trainers",
+            num: "hero.statistics.index1.number",
+            desc: "hero.statistics.index1.text",
         },
         {
-            num: 20,
-            desc: "Year Of Experience",
+            num: "hero.statistics.index2.number",
+            desc: "hero.statistics.index2.text",
         },
     ];
 }
