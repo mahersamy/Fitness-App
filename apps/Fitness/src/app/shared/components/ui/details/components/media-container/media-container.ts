900ff47f32a5bee1917997ccd892b01d2b3127bb
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-12-14 10:51:06 +0200

    feat: 🎸 (media-container)display shared selected meal&exercise

--- apps/Fitness/src/app/shared/components/ui/details/components/media-container/media-container.ts
+++ apps/Fitness/src/app/shared/components/ui/details/components/media-container/media-container.ts
@@ -1,7 +1,17 @@
-import {Component, input, InputSignal} from "@angular/core";
+import {isPlatformBrowser, NgOptimizedImage} from "@angular/common";
+import {
+    Component,
+    effect,
+    inject,
+    input,
+    InputSignal,
+    PLATFORM_ID,
+    signal,
+    WritableSignal,
+} from "@angular/core";
 import {Exercise} from "../../../../../models/exercises";
-import {Meal} from "../../../../../models/meals";
-import {NgOptimizedImage} from "@angular/common";
+import {Meal, MealDetails} from "../../../../../models/meals";
+import {MealService} from "../../../../../services/meals/meals";
 
 @Component({
     selector: "app-media-container",
@@ -10,7 +20,38 @@ import {NgOptimizedImage} from "@angular/common";
     styleUrl: "./media-container.scss",
 })
 export class MediaContainer {
+    private readonly _mealService = inject(MealService);
+    private readonly _PLATFORM_ID = inject(PLATFORM_ID);
+
     type = input.required<"meal" | "class">();
     selectedExercise: InputSignal<Exercise | undefined> = input<Exercise | undefined>(undefined);
     selectedMeal: InputSignal<Meal | undefined> = input<Meal | undefined>(undefined);
+
+    mealDetails: WritableSignal<MealDetails | undefined> = signal(undefined);
+
+    private levelsEffect = effect(() => {
+        if (this.selectedMeal() !== undefined) {
+            this.getMealDetails(this.selectedMeal()!.idMeal);
+        }
+    });
+
+    getMealDetails(meal_id: string) {
+        console.log(meal_id, "id");
+
+        this._mealService.getMealDetails(meal_id).subscribe({
+            next: (res) => {
+                this.mealDetails.set(res.meals[0]);
+                this._mealService.mealDetails.set(res.meals[0]);
+            },
+        });
+    }
+
+    openVideo() {
+        if (!isPlatformBrowser(this._PLATFORM_ID)) return;
+        if (this.type() === "class") {
+            window.open(this.selectedExercise()?.in_depth_youtube_explanation_link);
+        } else {
+            window.open(this.mealDetails()!.strSource);
+        }
+    }
 }
