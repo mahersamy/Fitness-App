a759977ef079538c4cc4e024c769b279f2a9c9d0
Reham-Gomaa <rehamgomaa686@gmail.com>
2026-01-08 18:11:01 +0200

    change details page to clientMode

--- apps/Fitness/src/app/app.routes.server.ts
+++ apps/Fitness/src/app/app.routes.server.ts
@@ -6,6 +6,10 @@ export const serverRoutes: ServerRoute[] = [
         renderMode: RenderMode.Prerender,
     },
     {
+        path: ":lang/main/details",
+        renderMode: RenderMode.Client,
+    },
+    {
         path: "**",
         renderMode: RenderMode.Server,
     },
